2bda459eb047427e880ef54f3ff2529a532390ff
PoscoBG <melissaskford@gmail.com>
2019-10-06 15:52:59 -0400

    Added PlayerMovement + filler sprite

---
+++ Trijam #39 Entry/Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(moveHorizontal * speed, moveVertical * speed, 0);
+    }
+}
